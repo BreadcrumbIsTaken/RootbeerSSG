5f6dc93f1a57f06d947da6dbb4d812c66f607992
BreadcrumbIsTaken <63469489+BreadcrumbIsTaken@users.noreply.github.com>
2021-05-28 11:58:49 -0700

    ⚙ Made it so it loads the sites config from a YAML file instead of the classes' parameters

--- rootbeer/__init__.py
+++ rootbeer/__init__.py
@@ -1,5 +1,4 @@
 # Python Standard Library Imports
-from typing import Optional
 from glob import glob
 from os import path
 from datetime import datetime
@@ -9,6 +8,7 @@ import pathlib
 from markdown import Markdown
 from jinja2 import Environment, FileSystemLoader, Template
 from slug import slug
+from yaml import safe_load
 
 # rootbeer Imports
 from .utils import *
@@ -17,100 +17,53 @@ from .errors import *
 
 class RootbeerSSG:
     def __init__(self,
-                 site_title: Optional[str] = 'RootbeerSSG!',
-                 pretty_permalinks_on_blog_posts: bool = False,
-                 pretty_permalinks_on_pages: bool = True,
-                 sort_posts_by: Optional[str] = 'date',
-                 sort_pages_by: Optional[str] = 'title',
-                 sort_posts_reverse: bool = True,
-                 sort_pages_reverse: bool = False,
-                 date_format_for_content='%m/%d/%y at %H:%M',
-                 content_directory: Optional[str] = 'rb_content',
-                 output_directory: Optional[str] = 'public',
-                 blog_directory: Optional[str] = 'blog',
-                 theme_name: Optional[str] = 'RBDefault',
-                 markdown_file_extention: Optional[str] = 'md',
-                 list_of_required_metadata_fields: Optional[list] = None,
-                 markdown_extentions: Optional[dict] = None,
-                 log_rootbeer_steps: bool = True,
+                 config_file: str = '.rbconfig'
                  ) -> None:
         """
         The class that genrates all the site's data and renders everything. The core or the module.
 
-        :param site_title: The title for your site. Default is "RootbeerSSG!"
-
-        :param pretty_permalinks_on_blog_posts: Do not add the date the post was written to the post's url.
-            Default is "False"
-
-        :param pretty_permalinks_on_pages: Do not add the date the page was written to the page's url.
-            Default is "True"
-
-        :param sort_posts_by: How to sort the posts. The options are determined by the content's metadata.
-            Default is "date"
-
-        :param sort_pages_by: How to sort the pages. The options are determined by the content's metadata.
-            Default is "title"
-
-        :param sort_posts_reverse: Sort the posts reverse or not. Default is "True". Works best if you sort by date.
-
-        :param sort_pages_reverse: Sort the pages reverse or not. Default is "False".
-
-        :param date_format_for_content: The way to format the date of the content. Uses the % time formatting notation.
-            Default is "%m/%d/%y at %H:%M", meaning "mm/dd/yy at H:M."
-            example: "5/20/21 at 13:57" will be parsed as "5/20/21 at 1:37 PM"
-
-        :param content_directory: The directory that contains the markdown files to parse. Default is "rb_content"
-
-        :param output_directory: The directory to export all rendered files. Default is "public"
-
-        :param blog_directory: The directory to put all the blog folders. Can be an emptry string if you want your blog
-            files to be in the root. Default is "blog"
-
-        :param markdown_file_extention: The file extention to use for your content. Default is "md"
-
-        :param list_of_required_metadata_fields: A list of all the required fields for content metadata. If empty or
-            None, then metadata is not required. Default is ["title", "date"]
-
-        :param markdown_extentions: The extentions to use for the markdown parser. You can find all the extentions
-            avaliable here: [INSERT GITHUB WIKI PAGE HERE.] The extentions will be installed automaticlly. The key is
-            the install name, the value is the import name.
-            Default: KEY: "markdown-full-yaml-metadata" VALUE: "full_yaml_metadata"
+        :param config_file: The config file for your site. Uses YAML syntax. Does not have to be a .rbconfig file.
+            Default: .rbconfig
         """
+
+        # ===== READ CONFIG =====
+        with open(config_file, 'r') as config_f:
+            self.config = safe_load(config_f.read())
 
         # ! Make a plugin that allow un-pretty permalinks so the date_format and pretty_permalinks params can be
         # ! Optional and make it so they are stated in a config file for the plugin to un-clutter the main class.
         # ===== MUTABLE PARAMS =====
-        if list_of_required_metadata_fields is None:
+        if 'list_of_required_metadata_fields' in self.config:
             # This just makes it so that the "title" metadata feild is required by default
             list_of_required_metadata_fields = ['title', 'date']
-        if markdown_extentions is None:
+        if 'markdown_extentions' in self.config:
             # This makes sure that the yaml markdown extentions is installed at all times.
             markdown_extentions = {'markdown-full-yaml-metadata': 'full_yaml_metadata'}
 
         # ===== GLOBAL VARIABLES =====
-        self.site_title: str = site_title
-        self.pretty_p: bool = pretty_permalinks_on_blog_posts
-        self.pretty_p_pages: bool = pretty_permalinks_on_pages
-        self.cont_dir: str = content_directory
-        self.out_dir: str = output_directory
-        self.blog_dir: str = blog_directory
-        self.theme: str = theme_name
-        self.themes_dir: str = 'themes'
-        self.date_format: str = date_format_for_content
+        self.site_title: str = self.config['site_title']
+        self.pretty_p: bool = self.config['pretty_permalinks_on_posts']
+        self.pretty_p_pages: bool = self.config['pretty_permalinks_on_pages']
+        self.cont_dir: str = self.config['content_directory']
+        self.out_dir: str = self.config['output_directory']
+        self.blog_dir: str = self.config['blog_directory']
+        self.theme: str = self.config['theme_name']
+        self.themes_dir: str = self.config['themes_dir']
+        self.date_format: str = self.config['date_format_for_content']
 
-        self.sort_pages: str = sort_pages_by
-        self.sort_pages_reversed: bool = sort_pages_reverse
-        self.sort_posts: str = sort_posts_by
-        self.sort_posts_reversed: bool = sort_posts_reverse
+        self.sort_pages: str = self.config['sort_pages_by']
+        self.sort_pages_reversed: bool = self.config['sort_pages_reverse']
+        self.sort_posts: str = self.config['sort_posts_by']
+        self.sort_posts_reversed: bool = self.config['sort_posts_reverse']
 
-        self.required_metadata_fields: list = list_of_required_metadata_fields
-        self.md_extentions: dict = markdown_extentions
+        self.required_metadata_fields: list = self.config['list_of_required_metadata_fields']
+        self.md_extentions: dict = self.config['markdown_extentions']
 
         self.content: list = list()
         self.list_of_files_generated: list = list()
         self.content_types: list = ['post', 'page']
 
-        self.log_steps = log_rootbeer_steps
+        self.log_steps = self.config['log_rootbeer_steps']
 
         # ===== VARIABLES =====
         list_of_extentions_for_markdown: list = list()
@@ -123,7 +76,7 @@ class RootbeerSSG:
 
         if self.log_steps:
             f'{Fore.MAGENTA}Installing markdown extentions. . .{Fore.RESET}'
-        rb_install_markdown_extras_modules(markdown_extentions.keys())
+        rb_install_markdown_extras_modules(self.config['markdown_extentions'].keys())
         if self.log_steps:
             print(f'{Fore.GREEN}Markdown extentions installed!{Fore.RESET}')
 
@@ -133,7 +86,7 @@ class RootbeerSSG:
         self.env: Environment = Environment(loader=FileSystemLoader(searchpath=search_path))
 
         # ===== OPTIONAL VARIABLES =====
-        self.md_ext: str = markdown_file_extention
+        self.md_ext: str = self.config['markdown_file_extention']
 
         # ===== FUNCTION CALLS =====
         rb_create_and_or_clean_path(self.out_dir)
