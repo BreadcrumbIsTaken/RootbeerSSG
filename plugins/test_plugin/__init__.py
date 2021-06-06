f87dff2d09acb3d5bcdb38b89de742baac6ac7af
BreadcrumbIsTaken <63469489+BreadcrumbIsTaken@users.noreply.github.com>
2021-06-05 17:55:21 -0700

    Added a little of inline docs for plugins

--- plugins/test_plugin/__init__.py
+++ plugins/test_plugin/__init__.py
@@ -1,6 +1,9 @@
 from rootbeer import signals
 
 
-@signals.during_content_load.connect
-def hello(sender) -> None:
-    print('hello! this was loaded during the content was loaded!')
+@signals.during_render_index.connect
+def hello(sender, *args, **kwargs) -> None:
+    print('hello! this was loaded during the index was rendered!')
+    print('You can get things needed for rendering from "sender! like this:')
+    print('"sender.content" will give you:')
+    print(sender.content)
