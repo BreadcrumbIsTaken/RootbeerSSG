62c15b8dea86d44f3db39aed6e4ee6eb646d7750
BreadcrumbIsTaken <63469489+BreadcrumbIsTaken@users.noreply.github.com>
2022-11-12 12:04:27 -0800

    Change name of package.
    Changed license.

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-from rootbeer import RootbeerSSG
+from rootbeerSSG import RootbeerSSG
 
 rb = RootbeerSSG(config_file='.rbconfig')
--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ def readme():
 
 
 setup(
-    name='rootbeer',
-    version='1.2.7',
+    name='rootbeerSSG',
+    version='1.0.0',
     description='The easy to use and very epic Static Site Generator for blogs!',
     long_description=readme(),
     long_description_content_type='text/markdown',
     url='https://github.com/BreadcrumbIsTaken/RootbeerSSG',
     author='Breadcrumb',
-    license='CC BY-SA 4.0',
+    license='MIT',
     classifiers=[
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.9',
@@ -26,7 +26,7 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Development Status :: 5 - Production/Stable'
     ],
-    packages=['rootbeer'],
+    packages=['rootbeerSSG'],
     include_package_data=True,
     install_requires=[
         'blinker',
